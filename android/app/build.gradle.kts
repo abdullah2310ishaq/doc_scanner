156c1ca42f7badf110450584f6190937285710a5
Funloft Development <devvunknown01@gmail.com>
2026-08-04 15:12:21 +0500

    4 Aug 2026 v3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.aidocumentscanner.pdfscanner.ocr.askpdf.smartcrop"
         minSdk = 24
         targetSdk = 36
-        versionCode = 95
-        versionName = "9.0.8"
+        versionCode = 96
+        versionName = "9.0.9"
     }
 
     signingConfigs {
